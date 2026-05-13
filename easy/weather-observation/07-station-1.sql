df1365b828f441d1b356c29eb0b2f0c26ef8450f
kk-dataanalytics <karo.kozak19@gmail.com>
2026-05-13 23:44:21 +0100

    Create 07-station-1.sql

---
+++ easy/weather-observation/07-station-1.sql
@@ -0,0 +1,8 @@
+-- Challenge: https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+-- Question:
+-- Query a list of CITY and STATE from the STATION table.
+
+-- Solution:
+SELECT CITY, STATE
+FROM STATION;
