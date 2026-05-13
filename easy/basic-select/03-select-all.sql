70c7d1706a3ec6070f9f2f0bf8477d564e7f2442
kk-dataanalytics <karo.kozak19@gmail.com>
2026-05-13 23:38:04 +0100

    Create 03-select-all.sql

---
+++ easy/basic-select/03-select-all.sql
@@ -0,0 +1,8 @@
+-- Challenge: https://www.hackerrank.com/challenges/select-all-sql/problem
+
+-- Question:
+-- Query all columns (attributes) for every row in the CITY table.
+
+-- Solution:
+SELECT *
+FROM CITY;
