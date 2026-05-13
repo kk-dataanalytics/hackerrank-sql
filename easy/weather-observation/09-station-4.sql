d240be839270866676dc5c99c609ee4d8ffaceba
kk-dataanalytics <karo.kozak19@gmail.com>
2026-05-13 23:53:16 +0100

    Create 09-station-4.sql

---
+++ easy/weather-observation/09-station-4.sql
@@ -0,0 +1,9 @@
+-- Challenge: https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
+-- Question:
+-- Find the difference between the total number of CITY entries
+-- in the table and the number of distinct CITY entries in the table.
+
+-- Solution:
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY)
+FROM STATION;
