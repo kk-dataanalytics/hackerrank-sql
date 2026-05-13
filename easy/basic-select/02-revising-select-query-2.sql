5247fdf9445738f57c4791935bf79137ebb85e60
kk-dataanalytics <karo.kozak19@gmail.com>
2026-05-13 23:34:45 +0100

    Create 02-revising-select-query-2.sql

---
+++ easy/basic-select/02-revising-select-query-2.sql
@@ -0,0 +1,12 @@
+-- Challenge: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+-- Question:
+-- Query the NAME field for all American cities in the CITY table
+-- with populations larger than 120000.
+-- The CountryCode for America is USA.
+
+-- Solution:
+SELECT NAME
+FROM CITY
+WHERE POPULATION > 120000
+AND COUNTRYCODE = 'USA';
