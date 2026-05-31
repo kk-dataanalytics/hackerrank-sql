a9ada2702486f215e0a929aa93cf357f0804b3d5
Karolina Kozak <karo.kozak19@gmail.com>
2026-06-01 00:43:57 +0100

    add count function solution — easy aggregations

--- easy/aggregations/01-revising-aggregations-count.sql
+++ easy/aggregations/01-revising-aggregations-count.sql
@@ -1,1 +1,14 @@
+-- Challenge: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
 
+-- Question:
+-- Query a count of the number of cities in CITY having a Population larger than 100,000
+
+-- Approach:
+-- Straightforward COUNT(*) with a WHERE filter
+-- First instinct was COUNT(ID) but COUNT(*) is cleaner
+-- since we just need the number of matching rows
+
+-- Solution:
+SELECT COUNT(*)
+FROM CITY
+WHERE POPULATION > 100000;
